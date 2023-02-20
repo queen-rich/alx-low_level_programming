565b21381bcc493729ae439dec06e9fdd6ab97ee
Adenuga Ronke <ademi@gmail.com>
2023-02-20 15:22:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Main - A program that pront a line using the printf function
+ * main - A program that print a line using the printf function
  * Return:0 (sucess)
  */
-int Main(void)
+int main(void)
 {
 	printf("with proper gramer but the outcome is a piece of art,*/ n");
 		return (0);
