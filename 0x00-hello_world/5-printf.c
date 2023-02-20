3c3fa4c1938b9d8bf34e1a5e1f8a1c578e9caa1a
Adenuga Ronke <ademi@gmail.com>
2023-02-20 15:00:13 -0800

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main . A program that pront a line using the printf function
+ * Return:0 (sucess)
+ */
+int Main(void)
+{
+	printf("with proper gramerbut the outcome is a piece of art,*/ n")
+		return (0);
+}
