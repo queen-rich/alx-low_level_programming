883d9dc4441ba204cfa15c76028e38fa81e6150f
Adenuga Ronke <ademi@gmail.com>
2023-02-20 16:30:48 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that print the size of various data types in c programming
+ * Return:0 (sucess)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+print("size of a char: %lu byte(s)\n", (unsigned long)size of (a));
+print("size of an int: %lu byte(s)\n", (unsigned long)size of (b));
+print("size of a long int: %lu byte(s)\n", (unsigned long)size of (c));
+print("size of a long long int: %lu byte(s)\n", (unsigned long)size of (d));
+print("size of a float: %lu byte(s)\n", (unsigned long) size of (f))
+	return (0);
+}
