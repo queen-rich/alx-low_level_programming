87de948ac5ec1796d94bb1d4e863c874d0466ac4
Adenuga Ronke <ademi@gmail.com>
2023-02-24 12:21:24 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 
 
 /**
- * mul -multipies two interger
+ * mul - multipies two integer
  * @x: the first number to multiply
  * @y: the second number to multiply with the first number
  * Return: the result of the multiplication between x param and y param
@@ -13,4 +13,5 @@ int mul(int x, int y)
 {
 
        	return (x*y);
+
 }
