70a9517f19ce54fbbb0f1ec3a4c98bc84f82ffd4
Adenuga Ronke <ademi@gmail.com>
2023-02-24 12:46:55 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print _number - print the number since 0 up to 9
+ * Return: the numbers since 0 up to 9
+ */
+
+vold print - number(void)
+{
+	int x;
+	for (x = 0; x <= 0; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('/n');
+}
+
